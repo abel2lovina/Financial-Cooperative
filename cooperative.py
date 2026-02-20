97de901a001b2068a2caef3842e7f7aee210df85
Abel Istifanus <abel2micro@gmail.com>
2026-02-20 14:39:14 +0100

    logout session

--- cooperative.py
+++ cooperative.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, session
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from datetime import datetime, timedelta
 import os
@@ -66,7 +66,7 @@ app.config['MAIL_PASSWORD'] = 'fuetixmcaqorygkg'
 
 mail = Mail(app)
 
-
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
 
 db.init_app(app)
 
@@ -102,7 +102,7 @@ def login():
         user = User.query.filter_by(email=request.form['email']).first()
         if user and user.check_password(request.form['password']):
             login_user(user)
-
+            session.permanent = True
             # Redirect based on role
             if user.role == 'admin':
                 return redirect(url_for('admin_dashboard'))
