e8863e62dc2376e5a745050885e40dbc9020d040
Abel Istifanus <abel2micro@gmail.com>
2026-02-21 10:05:56 +0100

    comit all

--- cooperative.py
+++ cooperative.py
@@ -653,4 +653,4 @@ def ping():
 if __name__ == '__main__':
     with app.app_context():
         port = int(os.environ.get("PORT", 10000))  # fallback to 10000 if PORT not set
-        app.run(host="0.0.0.0", port=port, debug=False)
+        app.run(host="0.0.0.0", port=port, debug=True)
