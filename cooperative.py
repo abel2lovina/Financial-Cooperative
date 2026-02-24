61fc9d8e99283e01b0bb10ea608a98032ab8ae47
Abel Istifanus <abel2micro@gmail.com>
2026-02-24 21:00:38 +0100

    remove create

--- cooperative.py
+++ cooperative.py
@@ -655,23 +655,7 @@ def logout():
 def ping():
     return "OK", 200
 
-with app.app_context():
-    db.create_all()
 
-    # Create admin if not exists
-    admin = User.query.filter_by(email="abel2micro@gmail.com").first()
-    if not admin:
-        admin = User(
-            full_name="System Admin",
-            email="abel2micro@gmail.com",
-            role="admin",
-            is_member=True,
-           
-        )
-        admin.set_password("abel")
-        db.session.add(admin)
-        db.session.commit()
-        print("Admin created successfully!")
 
 if __name__ == '__main__':
     with app.app_context():
